f11908cc54a12679dc042a48b28651bacf0f670d
sunhughees <471490816@qq.com>
2016-06-03 18:09:24 +0800

    deleted:    comments.tem 	modified:   src/helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,5 +1,94 @@
 
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('example')
-);
+// ReactDOM.render(
+// 	<h1>Hello, world!</h1>,
+// 	document.getElementById('example')
+// );
+
+var navigationConfig = [
+	{
+		href: 'http\://ryanclark.me',
+		text: 'My Website',
+		children: [
+			{
+				href:  'http\://ryanclark.me/how-angularjs-implements-dirty-checking/',
+				text: 'Angular Dirty Checking'
+			},
+			{
+				href: 'http\://ryanclark.me/getting-started-with-react/',
+				text: 'React'
+			},
+		]
+	}
+];
+
+var Navigation = React.createClass({
+	getInitialState: function () {
+		return {
+			openDropdown: -1
+		};
+	},
+
+	getDefaultProps: function () {
+		return {
+			config: []
+		}
+	},
+
+	openDropdown: function (id) {
+		this.setState({
+			openDropdown: id
+		});
+	},
+
+	closeDropdown: function () {
+		this.setState({
+			openDropdown: -1
+		});
+	},
+
+	propTypes: {
+		config: React.PropTypes.array
+	},
+
+	render: function () {
+		var config = this.props.config;
+		var items = config.map(function (item, index) {
+			var children, dropdown;
+			if (item.children) {
+				children = item.children.map(function (child) {
+					return (
+						<li className="navigation__dropdown__item">
+							<a className="navigation__dropdown__link" href={ child.href }>
+								{ child.text }
+							</a>
+						</li>
+					);
+				});
+				var dropdownClass = 'navigation__dropdown';
+				if (this.state.openDropdown === index) {
+					dropdownClass += 'navigation__dropdown__open';
+				}
+				console.log(this.state.openDropdown, index);
+				dropdown = (
+					<ul className={ dropdownClass }>
+						{ children }
+					</ul>
+				);
+			}
+			return (
+				<li className="navigation__item">
+					<a className="navigation__link" href={ item.href }>
+						{item.text}
+					</a>
+				</li>
+				);
+		});
+		return (
+			<div className="navigation">
+				{ items }
+			</div>
+			);
+	}
+});
+
+React.render(<Navigation config={ navigationConfig } />, document.body);

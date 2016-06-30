8477c7f4cc9466276f2931f3f05bef9d48a9f472
sven-sun <sunhughees@sina.cn>
2016-06-30 19:54:41 +0800

    add

---
+++ react-tutorials/2-react-router/src/js/client.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./components/Layout";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
+
